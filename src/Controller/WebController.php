1e2f0ae3612fd5e20af684ee95de377c69e37621
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2020-03-07 17:36:44 +0100

    Display version

--- src/Controller/WebController.php
+++ src/Controller/WebController.php
@@ -2,13 +2,42 @@
 
 namespace App\Controller;
 
+use Exception;
+use Psr\Cache\InvalidArgumentException;
+use Shivas\VersioningBundle\Service\VersionManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WebController extends AbstractController
 {
+    private $version_manager;
+    private $kernel;
+
+    public function __construct(VersionManager $v, KernelInterface $k)
+    {
+        $this->version_manager = $v;
+        $this->kernel = $k;
+    }
+
+    private function render_web_framework(string $ajax_landing) {
+        try {
+            $version = $this->version_manager->getVersion();
+            $is_debug_version =
+                $version->getPreRelease() && !(
+                    $version->getPreRelease() === 'rc' || substr($version->getPreRelease(), 0, 3) === 'rc.'
+                );
+        } catch (InvalidArgumentException $e) {
+            $is_debug_version = false;
+            $version = null;
+        }
+        return $this->render( 'web/framework.html.twig', [
+            'version' => $version, 'debug' => $is_debug_version, 'env' => $this->kernel->getEnvironment(),
+            'ajax_landing' => $ajax_landing
+        ] );
+    }
 
     /**
      * @Route("/")
@@ -16,9 +45,7 @@ class WebController extends AbstractController
      */
     public function framework(): Response
     {
-        return $this->render( 'web/framework.html.twig', [
-            'ajax_landing' => $this->generateUrl('initial_landing')
-        ] );
+        return $this->render_web_framework($this->generateUrl('initial_landing'));
     }
 
     /**
@@ -28,9 +55,7 @@ class WebController extends AbstractController
      */
     public function loader(string $ajax): Response
     {
-        return $this->render( 'web/framework.html.twig', [
-            'ajax_landing' => Request::createFromGlobals()->getBasePath() . '/jx/' . $ajax
-        ] );
+        return $this->render_web_framework(Request::createFromGlobals()->getBasePath() . '/jx/' . $ajax);
     }
 
 }
