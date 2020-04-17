ba38608a80bb8d3b9acf65f2adb7d1eeee3eb6e9
p-sincc <countcount.cc@gmail.com>
2020-04-17 23:09:59 +0200

    Adding SP requirements for town joining

--- src/Controller/GhostController.php
+++ src/Controller/GhostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Town;
 use App\Entity\TownClass;
+use App\Entity\User;
 use App\Response\AjaxResponse;
 use App\Service\ErrorHelper;
 use App\Service\GameFactory;
@@ -26,8 +27,30 @@ class GhostController extends AbstractController implements GhostInterfaceContro
      */
     public function welcome(EntityManagerInterface $em): Response
     {
+        /** @var User $user */
+        $user = $this->getUser();
+        $available_town_classes = [
+            'small' => false,
+            'remote' => false,
+            'panda' => false,
+            'custom' => false,
+        ];
+        if ($user->getSoulPoints() < 100 || $user->getSoulPoints() >= 500) {
+            $available_town_classes['small'] = true;
+        }
+        if ($user->getSoulPoints() >= 100) {
+            $available_town_classes['remote'] = true;
+        }
+        if ($user->getSoulPoints() >= 500) {
+            $available_town_classes['panda'] = true;
+        }
+        if ($user->getSoulPoints() >= 1000) {
+            $available_town_classes['custom'] = true;
+        }
+
         return $this->render( 'ajax/ghost/intro.html.twig', [
-            'townClasses' => $em->getRepository(TownClass::class)->findAll()
+            'townClasses' => $em->getRepository(TownClass::class)->findAll(),
+            'userCanJoin' => $available_town_classes,
         ] );
     }
 
@@ -51,14 +74,14 @@ class GhostController extends AbstractController implements GhostInterfaceContro
         $citizen = $factory->createCitizen($town, $user, $error);
         if (!$citizen) return AjaxResponse::error($error);
 
-//        try {
+        try {
             $em->persist($town);
             $em->persist($citizen);
             $em->flush();
-/*        } catch (Exception $e) {
+        } catch (Exception $e) {
             return AjaxResponse::error(ErrorHelper::ErrorDatabaseException);
         }
-*/
+
         return AjaxResponse::success();
     }
 
