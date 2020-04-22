38bac511aaff8cb9fa3aed70cddd2d0269e8a6cb
p-sincc <countcount.cc@gmail.com>
2020-04-23 01:07:10 +0200

    Improving forum controls

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -416,6 +416,7 @@ class ForumController extends AbstractController
             'fid' => $id,
             'tid' => null,
             'pid' => null,
+            'username' => $this->getUser()->getUsername(),
         ] );
     }
 
