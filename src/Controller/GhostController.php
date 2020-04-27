465eba9892e93907ffa948cca219554c3262737a
ludofloria <ludofloria@aifesdespets.fr>
2020-04-27 11:37:55 +0200

    Added auto town generation (it might be optimized though)

--- src/Controller/GhostController.php
+++ src/Controller/GhostController.php
@@ -12,6 +12,7 @@ use App\Service\GameFactory;
 use App\Service\JSONRequestParser;
 use App\Structures\Conf;
 use App\Structures\MyHordesConf;
+use App\Structures\TownConf;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,8 +31,6 @@ class GhostController extends AbstractController implements GhostInterfaceContro
      */
     public function welcome(EntityManagerInterface $em, ConfMaster $conf): Response
     {
-
-
         return $this->render( 'ajax/ghost/intro.html.twig', [
             'townClasses' => $em->getRepository(TownClass::class)->findAll(),
             'userCanJoin' => $this->getUserTownClassAccess($conf->getGlobalConf()),
@@ -66,6 +65,55 @@ class GhostController extends AbstractController implements GhostInterfaceContro
         $citizen = $factory->createCitizen($town, $user, $error);
         if (!$citizen) return AjaxResponse::error($error);
 
+        // Let's check if there is enough opened town
+        $openTowns = $em->getRepository(Town::class)->findOpenTown();
+        $count = array(
+            "fr" => array(
+                "remote" => 0,
+                "panda" => 0,
+                "small" => 0
+            ),
+            "de" => array(
+                "remote" => 0,
+                "panda" => 0,
+                "small" => 0
+            ),
+            "en" => array(
+                "remote" => 0,
+                "panda" => 0,
+                "small" => 0
+            ),
+            "es" => array(
+                "remote" => 0,
+                "panda" => 0,
+                "small" => 0
+            ),
+        );
+        foreach ($openTowns as $openTown) {
+            $count[$openTown->getLanguage()][$openTown->getType()->getName()]++;
+        }
+
+        $minOpenTown = $this->getMinOpenTownClass($conf->getGlobalConf());
+
+        foreach ($count as $townLang => $array) {
+            foreach ($array as $townClass => $openCount) {
+                if($openCount < $minOpenTown[$townClass]){
+                    // Fetch min/max pop from config for this townClass
+                    $class = $em->getRepository(TownClass::class)->findOneByName($townClass);
+                    $townTpl = new Town();
+                    $townTpl->setType($class);
+                    $townConf = $conf->getTownConfiguration($townTpl);
+                    $min = $townConf->get(TownConf::CONF_POPULATION_MIN, 0);
+                    $max = $townConf->get(TownConf::CONF_POPULATION_MAX, 0);
+                    // Create the count we need
+                    for($i = 0 ; $i < $minOpenTown[$townClass] - $openCount ; $i++){
+                        $newTown = $factory->createTown(null, $townLang, mt_rand($min, $max), $townClass);
+                        $em->persist($newTown);
+                    }
+                }
+            }
+        }
+
         try {
             $em->persist($town);
             $em->persist($citizen);
@@ -87,6 +135,15 @@ class GhostController extends AbstractController implements GhostInterfaceContro
             'remote' => ($user->getSoulPoints() >= $conf->get( MyHordesConf::CONF_SOULPOINT_LIMIT_REMOTE, 100 )),
             'panda' => ($user->getSoulPoints() >= $conf->get( MyHordesConf::CONF_SOULPOINT_LIMIT_PANDA, 500 )),
             'custom' => ($user->getSoulPoints() >= $conf->get( MyHordesConf::CONF_SOULPOINT_LIMIT_CUSTOM, 1000 )),
+        ];
+    }
+
+    public function getMinOpenTownClass(MyHordesConf $conf): array {
+        return [
+            'small' => $conf->get( MyHordesConf::CONF_TOWNS_OPENMIN_SMALL, 2 ),
+            'remote' => $conf->get( MyHordesConf::CONF_TOWNS_OPENMIN_REMOTE, 4 ),
+            'panda' => $conf->get( MyHordesConf::CONF_TOWNS_OPENMIN_SMALL, 2 ),
+            'custom' => $conf->get( MyHordesConf::CONF_TOWNS_OPENMIN_CUSTOM, 0 ),
         ];
     }
 
