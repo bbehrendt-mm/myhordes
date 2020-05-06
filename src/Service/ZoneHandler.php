d8f36142a992ad96f41d9a1d95704aa9891cead1
Choreas <niklas.kosanke@web.de>
2020-05-06 20:04:20 +0200

    fix ZoneHandler

--- src/Service/ZoneHandler.php
+++ src/Service/ZoneHandler.php
@@ -314,7 +314,7 @@ class ZoneHandler
                 $this->entity_manager->remove( $et );
             foreach ($this->entity_manager->getRepository(TownLogEntry::class)->findByFilter( $zone->getTown(), null, null, $zone, null, null ) as $entry)
                 /** @var TownLogEntry $entry */
-                if ($entry->getClass() !== TownLogEntry::ClassCritical)
+                if ($entry->getLogEntryTemplate()->getClass() !== TownLogEntry::ClassCritical)
                     $this->entity_manager->remove( $entry );
         }
         // If zombies can take control after leaving the zone and there are citizens remaining, install a grace escape timer
