8f331e126fcef2b7e7ab62271932b8e0f8a22fc6
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2020-06-17 18:29:44 +0200

    oops

--- src/Service/DeathHandler.php
+++ src/Service/DeathHandler.php
@@ -112,7 +112,7 @@ class DeathHandler
         $citizen->setCauseOfDeath($cod);
         $citizen->setAlive(false);
 
-        $gazette = $this->entity_manager->getRepository(Gazette::class)->findOneByTownAndDay($citizen->getTown(), ($citizen->getTown()->getDay() + ($cod->getId() == CauseOfDeath::NightlyAttack ? 0 : 1)));
+        $gazette = $citizen->getTown()->findGazette( ($citizen->getTown()->getDay() + ($cod->getId() == CauseOfDeath::NightlyAttack ? 0 : 1)) );
         if($gazette !== null){
             $gazette->addVictim($citizen);
             $this->entity_manager->persist($gazette);
