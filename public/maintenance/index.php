71c0c8de6bb89996b0a7f351733c00bbc9c77c13
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2020-06-30 17:49:20 +0200

    Fixed issue preventing maintenance screen to render if the current work dir was not set within /maintenance

--- public/maintenance/index.php
+++ public/maintenance/index.php
@@ -5,6 +5,8 @@
     }
 
     header('X-AJAX-Control: reset');
+
+    $f = function(string $ff,$type): string { return "url(data:image/{$type};base64," . base64_encode( file_get_contents( __DIR__ . '/' . $ff ) ) . ')'; }
 ?>
 <html lang="en">
 <head>
@@ -23,36 +25,36 @@
     <style>
         body { background: black; font-family: sans-serif; }
         #header, #content, #footer { position: relative; margin: 0 auto; padding: 0; width: 950px; overflow: visible; }
-        #header  { height: 140px; background: url(data:image/png;base64,<?=base64_encode( file_get_contents( 'deco_header.png' ) ) ?>) left top no-repeat; }
-        #content { background: url(data:image/jpeg;base64,<?=base64_encode( file_get_contents( 'bg_content.jpg' ) ) ?>) left top repeat-y }
-        #footer  { height: 14px; background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'bg_content_footer.gif' ) ) ?>) left top no-repeat }
+        #header  { height: 140px; background: <?=$f('deco_header.png','png')?> left top no-repeat; }
+        #content { background: <?=$f('bg_content.jpg','jpeg')?> left top repeat-y }
+        #footer  { height: 14px; background: <?=$f('bg_content_footer.gif','gif')?> left top no-repeat }
 
         #button { position: absolute; height: 46px; width: 137px; top: 51px; left: 409px; cursor: pointer;
-            background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'deco_jouerBt.gif' ) ) ?>) center no-repeat;
+            background: <?=$f('deco_jouerBt.gif','gif')?> center no-repeat;
         }
         #sparks { position: absolute; height: 38px; width: 166px; top: 6px; left: 395px; cursor: pointer;
-            background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'electrik.gif' ) ) ?>) center no-repeat;
+            background: <?=$f('electrik.gif','gif')?> center no-repeat;
         }
-        #button:hover { background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'deco_jouerBt2.gif' ) ) ?>) center no-repeat; }
+        #button:hover { background: <?=$f('deco_jouerBt2.gif','gif')?> center no-repeat; }
         #button>div {
             text-align: center; text-transform: uppercase; font-size: 19pt; padding-top: 5px; font-weight: bolder; color: #f0d79e;
-            text-shadow: 0px 2px 0px #94361b, 0px -2px 0px #94361b, 2px 0px 0px #94361b, -2px 0px 0px #94361b, 2px 2px 0px #94361b, -2px -2px 0px #94361b, -2px 2px 0px #94361b;
+            text-shadow: 0 2px 0 #94361b, 0 -2px 0 #94361b, 2px 0 0 #94361b, -2px 0 0 #94361b, 2px 2px 0 #94361b, -2px -2px 0 #94361b, -2px 2px 0 #94361b;
         }
         #button:hover>div { padding-top: 8px; }
 
         #content-header, #content-content, #content-footer { position: relative; margin: 0 auto; padding: 0; width: 600px; overflow: hidden; }
-        #content-header  { height: 18px; background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'panel_header.gif' ) ) ?>) left top no-repeat; }
-        #content-content { background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'panel_bg.gif' ) ) ?>) left top repeat-y; }
-        #content-footer  { height: 16px; background: url(data:image/gif;base64,<?=base64_encode( file_get_contents( 'panel_footer.gif' ) ) ?>) left top no-repeat }
+        #content-header  { height: 18px; background: <?=$f('panel_header.gif','gif')?> left top no-repeat; }
+        #content-content { background: <?=$f('panel_bg.gif','gif')?> left top repeat-y; }
+        #content-footer  { height: 16px; background: <?=$f('panel_footer.gif','gif')?> left top no-repeat }
 
         #content-content>div { padding: 2px 20px; }
         h1 { color: #f0d79e; border-bottom: 1px solid #f0d79e; font-size: 1.4em; }
         h1:not(:first-child) { margin: 45px 0 0; }
         p.text { text-align: justify; color: white; padding-left: 48px; }
-        p.en { background: url(data:image/png;base64,<?=base64_encode( file_get_contents( 'en.png' ) ) ?>) left top no-repeat }
-        p.fr { background: url(data:image/png;base64,<?=base64_encode( file_get_contents( 'fr.png' ) ) ?>) left top no-repeat }
-        p.de { background: url(data:image/png;base64,<?=base64_encode( file_get_contents( 'de.png' ) ) ?>) left top no-repeat }
-        p.es { background: url(data:image/png;base64,<?=base64_encode( file_get_contents( 'es.png' ) ) ?>) left top no-repeat }
+        p.en { background: <?=$f( 'en.png', 'png')?> left top no-repeat }
+        p.fr { background: <?=$f( 'fr.png', 'png')?> left top no-repeat }
+        p.de { background: <?=$f( 'de.png', 'png')?> left top no-repeat }
+        p.es { background: <?=$f( 'es.png', 'png')?> left top no-repeat }
     </style>
 </head>
 <body>
