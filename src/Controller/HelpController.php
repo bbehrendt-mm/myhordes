6e2a47992de5c740aefb88b89a1a6452245490f6
ludofloria <ludofloria@aifesdespets.fr>
2020-08-13 10:13:51 +0200

    Started real work on templates for the help section

---
+++ src/Controller/HelpController.php
@@ -0,0 +1,45 @@
+<?php /** @noinspection PhpComposerExtensionStubsInspection */
+
+namespace App\Controller;
+
+use App\Response\AjaxResponse;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * @Route("/",condition="request.isXmlHttpRequest()")
+ */
+class HelpController extends AbstractController
+{
+    protected $entity_manager;
+    protected $translator;
+
+    public function __construct(EntityManagerInterface $em, TranslatorInterface $translator)
+    {
+        $this->entity_manager = $em;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @Route("jx/help/{name}", name="help")
+     * @param Request $request
+     * @return Response
+     */
+    public function soul_news(Request $request, string $name = 'welcome'): Response
+    {
+        try {
+            return $this->render( "ajax/help/$name.html.twig", ['section' => $name]);
+        } catch (Exception $e){
+            return $this->redirect($this->generateUrl('help'));
+        }
+    }
+}
