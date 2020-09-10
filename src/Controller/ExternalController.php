77936b8680da7d69bcdebd0ee566b719c777a33f
Julian EDELIN <930873-Xemaro@users.noreply.gitlab.com>
2020-09-10 10:28:16 +0000

    Resolve "The game doesn't return a JSON during maintenance"

--- src/Controller/ExternalController.php
+++ src/Controller/ExternalController.php
@@ -144,8 +144,8 @@
                 break;
                 case $type === 'status':
                     $data = [
-                        "attack" => false,
-                        "maintain" => false
+                        "attack" => $this->time_keeper->isDuringAttack(),
+                        "maintain" => is_file($this->getParameter('kernel.project_dir')."/public/maintenance/.active")
                     ];
                 break;
                 case $type === 'items':
