0bbf5fbb652963ed93d3fd29f111ee243b5f17cf
Dylan57 <dylandep57@gmail.com>
2020-09-23 20:43:05 +0200

    Corrected night bonus for time between 00:00 to 07:00

--- src/Service/CitizenHandler.php
+++ src/Service/CitizenHandler.php
@@ -624,7 +624,7 @@ class CitizenHandler
         $camping_values['night'] = 0;
         $camping_datetime = new DateTime();
         $camping_datetime->setTimestamp( $citizen->getCampingTimestamp() );
-        if ($camping_datetime->format('G') >= 19) {
+        if ($camping_datetime->format('G') >= 19 || $camping_datetime->format('G') < 7) {
             $camping_values['night'] = 2;
         }
 
