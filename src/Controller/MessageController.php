3ed420ffb3e9c9d770b3b637429a46f020199c99
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2020-11-14 15:26:04 +0100

    Fix error when linking to the last post of a thread page

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -912,19 +912,19 @@ class MessageController extends AbstractController
         $marker = $em->getRepository(ThreadReadMarker::class)->findByThreadAndUser( $user, $thread );
         if (!$marker) $marker = (new ThreadReadMarker())->setUser($user)->setThread($thread);
 
-        if ($user->getRightsElevation() >= User::ROLE_CROW)
+        if ($this->perm->isPermitted( $permissions, ForumUsagePermissions::PermissionModerate ))
             $pages = floor(max(0,$em->getRepository(Post::class)->countByThread($thread)-1) / $num_per_page) + 1;
         else
             $pages = floor(max(0,$em->getRepository(Post::class)->countUnhiddenByThread($thread)-1) / $num_per_page) + 1;
 
         if ($jump_post)
-            $page = min($pages,1 + floor((1+$em->getRepository(Post::class)->getOffsetOfPostByThread( $thread, $jump_post )) / $num_per_page));
+            $page = min($pages,1 + floor(($em->getRepository(Post::class)->getOffsetOfPostByThread( $thread, $jump_post, $this->perm->isPermitted( $permissions, ForumUsagePermissions::PermissionModerate ) )) / $num_per_page));
         elseif ($parser->has('page'))
             $page = min(max(1,$parser->get('page', 1)), $pages);
         elseif (!$marker->getPost()) $page = 1;
-        else $page = min($pages,1 + floor((1+$em->getRepository(Post::class)->getOffsetOfPostByThread( $thread, $marker->getPost() )) / $num_per_page));
+        else $page = min($pages,1 + floor(($em->getRepository(Post::class)->getOffsetOfPostByThread( $thread, $marker->getPost(), $this->perm->isPermitted( $permissions, ForumUsagePermissions::PermissionModerate ) )) / $num_per_page));
 
-        if ($user->getRightsElevation() >= User::ROLE_CROW)
+        if ($this->perm->isPermitted( $permissions, ForumUsagePermissions::PermissionModerate ))
             $posts = $em->getRepository(Post::class)->findByThread($thread, $num_per_page, ($page-1)*$num_per_page);
         else
             $posts = $em->getRepository(Post::class)->findUnhiddenByThread($thread, $num_per_page, ($page-1)*$num_per_page);
