3d1007a2cc10f4fc0707a8bd8ec3d95e59fc7baf
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2020-12-04 17:55:16 +0100

    Fix night bonus display for camping chance preview

--- src/Service/CitizenHandler.php
+++ src/Service/CitizenHandler.php
@@ -622,7 +622,8 @@ class CitizenHandler
         // Night time bonus.
         $camping_values['night'] = 0;
         $camping_datetime = new DateTime();
-        $camping_datetime->setTimestamp( $citizen->getCampingTimestamp() );
+        if ($citizen->getCampingTimestamp() > 0)
+            $camping_datetime->setTimestamp( $citizen->getCampingTimestamp() );
         if ($camping_datetime->format('G') >= 19 || $camping_datetime->format('G') < 7) {
             $camping_values['night'] = 2;
         }
