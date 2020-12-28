5f1c44aa008922ca967677a3c27e0a0898ff9e71
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2020-12-28 20:32:40 +0100

    Display message content and editor, WIP

--- src/Controller/Messages/MessageGlobalPMController.php
+++ src/Controller/Messages/MessageGlobalPMController.php
@@ -6,6 +6,7 @@ use App\Entity\ActionCounter;
 use App\Entity\AdminReport;
 use App\Entity\Citizen;
 use App\Entity\Complaint;
+use App\Entity\ForumModerationSnippet;
 use App\Entity\ForumUsagePermissions;
 use App\Entity\Item;
 use App\Entity\ItemPrototype;
@@ -51,6 +52,30 @@ class MessageGlobalPMController extends MessageController
         return $this->render( 'ajax/pm/view.html.twig', $this->addDefaultTwigArgs(null, [
 
         ] ));
+    }
+
+    /**
+     * @Route("jx/pm/post-editor", name="pm_thread_editor_controller")
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function editor_pm_thread_api(EntityManagerInterface $em): Response {
+        return $this->render( 'ajax/forum/editor.html.twig', [
+            'fid' => null,
+            'tid' => null,
+            'pid' => null,
+
+            'permission' => $this->getPermissionObject( ForumUsagePermissions::PermissionCreatePost ),
+            'snippets' => $this->isGranted('ROLE_CROW') ? $this->entity_manager->getRepository(ForumModerationSnippet::class)->findAll() : [],
+
+            'emotes' => $this->getEmotesByUser($this->getUser(),true),
+            'username' => $this->getUser()->getName(),
+            'forum' => false,
+            'town_controls' => null,
+
+            'type' => 'global-pm',
+            'target_url' => 'town_house_send_pm_controller',
+        ] );
     }
 
 }
