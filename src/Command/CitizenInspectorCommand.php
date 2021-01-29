8a5115619584c6bedb6a8659eb8fb90336788228
Ludofloria <ludofloria@aifesdespets.fr>
2021-01-29 15:11:08 +0100

    Improved set-ap cmdlet

--- src/Command/CitizenInspectorCommand.php
+++ src/Command/CitizenInspectorCommand.php
@@ -109,6 +109,11 @@ class CitizenInspectorCommand extends Command
         if ($set_ap >= 0) {
             $citizen->setAp( $set_ap );
             $updated = true;
+            if($set_ap > 0) {
+                $this->citizenHandler->inflictStatus($citizen, "tired");
+            } else {
+                $this->citizenHandler->removeStatus($citizen, "tired");
+            }
         }
 
         $set_pm = $input->getOption('set-pm');
