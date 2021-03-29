dca7900eb1a4e400aa96955bd11cc8f9f46b4a82
Benoit <eragony.hordes@gmail.com>
2021-03-29 16:29:16 +0000

    Update ExternalController.php

--- src/Controller/ExternalController.php
+++ src/Controller/ExternalController.php
@@ -401,9 +401,10 @@ class ExternalController extends InventoryAwareController {
                     }
                     break;
                 case 'dried':
-                if ($zoneOfUser) {
-                    $data[$field] = $zone->getRuinDigs() <= 0;
-                }
+                    if ($zoneOfUser) {
+                        $data[$field] = $zone->getRuinDigs() <= 0;
+                    }
+                    break;
             }
         }
 
