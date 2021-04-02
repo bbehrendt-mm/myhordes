b09daba4768d3d9c009dbb77dd5a2d00ddfad544
Dylan57 <dylandep57@gmail.com>
2021-04-02 17:15:41 +0200

    Removed lvl4 of Public Lights

--- src/Service/ZoneHandler.php
+++ src/Service/ZoneHandler.php
@@ -195,7 +195,7 @@ class ZoneHandler
                             // Novelty Lamps are at lv2 and the zone distance is above 10km; apply malus
                             elseif ($novelty_lamps->getLevel() === 2 && $zone->getDistance() > 10.0) $factor -= $night_mode_malue;
                             // Novelty Lamps are at lv4 and the zone distance is within 10km; apply bonus
-                            elseif ($novelty_lamps->getLevel() === 4 && $zone->getDistance() <= 10.0) $factor += 0.2;
+                            // elseif ($novelty_lamps->getLevel() === 4 && $zone->getDistance() <= 10.0) $factor += 0.2;
 
                         } else $factor -= $night_mode_malue; // Night mode is active; apply malus
 
