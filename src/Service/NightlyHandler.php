ce8d62055121fde1aaf6a618f23506b2444bd41e
Benjamin Behrendt <behrendt@isg.cs.uni-magdeburg.de>
2021-04-04 17:00:18 +0200

    Revert "Fixes #1064"
    This reverts commit 848de580

--- src/Service/NightlyHandler.php
+++ src/Service/NightlyHandler.php
@@ -651,7 +651,7 @@ class NightlyHandler
         }
 
         if ($total_watch_def > 0 && $overflow > 0) {
-            $this->entity_manager->persist($this->logTemplates->nightlyAttackWatchersZombieStopped($town, min($overflow, $total_watch_def), count($watchers)));
+            $this->entity_manager->persist($this->logTemplates->nightlyAttackWatchersZombieStopped($town, min($overflow, $total_watch_def)));
         }
 
         $initial_overflow = $overflow;
