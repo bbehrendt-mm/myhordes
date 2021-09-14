34b44bc3c1e949f6ad7c4269aaca7dc3d282c879
Ludofloria <ludofloria@aifesdespets.fr>
2021-09-14 14:34:48 +0200

    Implemented #1499

--- src/Entity/Emotes.php
+++ src/Entity/Emotes.php
@@ -49,6 +49,11 @@ class Emotes {
      */
     private $orderIndex;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $i18n = false;
+
     public function getOrderIndex(): ?int {
         return $this->orderIndex;
     }
@@ -65,24 +70,46 @@ class Emotes {
         return $this->path;
     }
 
-    public function setOrderIndex(int $value) {
+    public function setOrderIndex(int $value): self {
         $this->orderIndex = $value;
+
+        return $this;
     }
 
-    public function setTag(string $value) {
+    public function setTag(string $value): self {
         $this->tag = $value;
+
+        return $this;
     }
 
-    public function setPath(string $value) {
+    public function setPath(string $value): self {
         $this->path = $value;
+
+        return $this;
     }
 
-    public function setIsActive(bool $value) {
+    public function setIsActive(bool $value): self {
         $this->isActive = $value;
+
+        return $this;
     }
 
-    public function setRequiresUnlock(bool $value) {
+    public function setRequiresUnlock(bool $value): self {
         $this->requiresUnlock = $value;
+
+        return $this;
+    }
+
+    public function getI18n(): ?bool
+    {
+        return $this->i18n;
+    }
+
+    public function setI18n(bool $i18n): self
+    {
+        $this->i18n = $i18n;
+
+        return $this;
     }
 
 }
