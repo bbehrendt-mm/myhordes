345555aec9f251ff1487894baaa8ffaea7888214
Benjamin Behrendt <brainbox@zombvival.de>
2021-12-04 14:50:20 +0100

    Add ThreadTag instance

---
+++ src/Entity/ThreadTag.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThreadTagRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\UniqueConstraint;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=ThreadTagRepository::class)
+ * @UniqueEntity("name")
+ * @Table(uniqueConstraints={
+ *     @UniqueConstraint(name="thread_tag_name_unique",columns={"name"})
+ * })
+ */
+class ThreadTag
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=16)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="binary")
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $permissionMap;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getPermissionMap(): ?int
+    {
+        return $this->permissionMap;
+    }
+
+    public function setPermissionMap(?int $permissionMap): self
+    {
+        $this->permissionMap = $permissionMap;
+
+        return $this;
+    }
+}
