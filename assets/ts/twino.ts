cb6c163ecef3a2822cb96ceeadefe443424c89e5
brainbox <wvsgnovize@gmail.com>
2022-04-02 14:50:17 +0200

    Fixes #2001

--- assets/ts/twino.ts
+++ assets/ts/twino.ts
@@ -80,7 +80,7 @@ class TwinoRegexResult {
 
         switch (type) {
             case TwinoRegexResult.TypeShortBB: return /(?:([^\w\s]){2})([\s\S]*?)\1{2}/gm;
-            case TwinoRegexResult.TypeInset:   return /{([a-zA-Z]+)}|{([a-zA-Z]+),([\w,]*)}|{([a-zA-Z]+)(\d+)}|(?:\B|\b)@([\w_-]+)(?::(\d+))?(?:\B|\b)|(?:\B|\b)@(?::(\d+))(?:\B|\b)/g;
+            case TwinoRegexResult.TypeInset:   return /{([a-zA-Z]+)}|{([a-zA-Z]+),([\w,]*)}|{([a-zA-Z]+)(\d+)}|(?:\B|\b)@([\w_-]+)(?::(\d+))?(?:\B|\b)/g;
             case TwinoRegexResult.TypeEmote:   return /(?::(\w+?):)|([:;].)/g;
             default: throw Error( 'No regex defined for this type of TRR!' )
         }
