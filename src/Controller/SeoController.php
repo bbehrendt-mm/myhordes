9b8c5922bede40bf6debb82e700600470e58cf51
brainbox <wvsgnovize@gmail.com>
2022-04-02 14:21:06 +0200

    Improvements to the SEO system

--- src/Controller/SeoController.php
+++ src/Controller/SeoController.php
@@ -44,6 +44,15 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class SeoController extends CustomAbstractController
 {
 
+    private function renderWithLanguageLinks( Request $request, string $view, array $data ): Response {
+        $routes = [];
+        if ($request->get('lang', null) && $request->get('_route', null))
+            foreach (['de','en','fr','es'] as $lang) if ($lang !== $request->get('lang'))
+                $routes[$lang] = $this->generateUrl( $request->get('_route'), array_merge( $request->get('_route_params'), ['lang' => $lang] ), UrlGeneratorInterface::ABSOLUTE_URL );
+
+        $data = array_merge( $data, ['in_other_languages' => $routes] );
+        return $this->render( $view, $data );
+    }
 
     /**
      * @Route("/robots.txt", name="robots.txt")
@@ -51,7 +60,6 @@ class SeoController extends CustomAbstractController
      */
     public function robots_txt(): Response
     {
-
         $base_url = Request::createFromGlobals()->getHost();
         return new Response(
             "# robots.txt for {$base_url}\n" .
@@ -61,17 +69,31 @@ class SeoController extends CustomAbstractController
             , 200, ['Content-Type' => 'text/plain']);
     }
 
-
     /**
      * @Route(
      *     "/", priority=100, name="seo_welcome",
      *     condition="request.headers.get('User-Agent') matches '%seobots%'"
      * )
+     * @param string $lang
      * @return Response
      */
     public function seo_welcome(): Response
     {
-        return $this->render( 'seo/welcome.html.twig', [
+        return $this->redirectToRoute( 'seo_welcome_lang', ['lang' => 'en'] );
+    }
+
+    /**
+     * @Route(
+     *     "/{lang}/", priority=100, name="seo_welcome_lang",
+     *     requirements={"lang"="de|en|fr|es"},
+     *     condition="request.headers.get('User-Agent') matches '%seobots%'"
+     * )
+     * @param Request $request
+     * @return Response
+     */
+    public function seo_welcome_with_lang(Request $request): Response
+    {
+        return $this->renderWithLanguageLinks( $request, 'seo/welcome.html.twig', [
             'devs' => WebController::$devs,
             'supporters' => WebController::$supporters
         ] );
@@ -87,21 +109,52 @@ class SeoController extends CustomAbstractController
      */
     public function seo_help(string $name = 'welcome'): Response
     {
+        return $this->redirectToRoute( 'seo_help_lang', [ 'lang' => 'en', 'name' => $name ] );
+    }
+
+    /**
+     * @Route(
+     *     "{lang}/help/{name}", priority=100, name="seo_help_lang", requirements={"lang"="de|en|fr|es"},
+     *     condition="request.headers.get('User-Agent') matches '%seobots%'"
+     * )
+     * @param Request $request
+     * @param string $name
+     * @return Response
+     */
+    public function seo_help_with_lang(Request $request, string $lang, string $name = 'welcome'): Response
+    {
         if ($name === 'shell') return $this->redirect($this->generateUrl('help'));
 
         try {
             $twig = $this->container->get('twig');
             $template = $twig->load( "ajax/help/$name.html.twig" );
             $content = $template->renderBlock( 'helpContent', []);
+            try {
+                $title = $template->renderBlock( 'title', []);
+            } catch (\Throwable $t) { $title = null; }
+
         } catch (\Throwable $e){
-            return $this->redirect($this->generateUrl('help'));
+            return $this->redirect($this->generateUrl('seo_help_lang', ['lang' => $lang]));
         }
 
-        return $this->render( 'seo/help.html.twig', [
+        return $this->renderWithLanguageLinks( $request, 'seo/help.html.twig', [
+            'title' => $title,
             'devs' => WebController::$devs,
             'supporters' => WebController::$supporters,
             'section' => $name,
             'content' => str_replace('x-ajax-href', 'href', $content)
         ] );
+    }
+
+    /**
+     * @Route("{lang}/{any}", name="seo_redirect", requirements={"lang"="de|en|fr|es", "any"=".*"})
+     * @param Request $request
+     * @param string $lang
+     * @param string $any
+     * @return Response
+     */
+    public function seo_redirect(Request $request, string $lang, string $any): Response
+    {
+        return $this->redirect( "{$request->getScheme()}://{$request->getHost()}/jx/{$any}" );
     }
 }
