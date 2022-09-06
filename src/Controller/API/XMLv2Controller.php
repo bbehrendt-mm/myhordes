09c63f5b133b66069efb48e40b1648c860048c1c
Ludofloria <ludofloria@aifesdespets.fr>
2022-09-06 17:38:28 +0200

    Do not display dead citizen along with alive ones (issue #2412)

--- src/Controller/API/XMLv2Controller.php
+++ src/Controller/API/XMLv2Controller.php
@@ -570,27 +570,27 @@ class XMLv2Controller extends CoreController {
             // Citizens
             foreach($town->getCitizens() as $citizen){
                 /** @var Citizen $citizen */
-                $citizenNode = [
-                    'attributes' => [
-                        'dead' => (int)!$citizen->getAlive(),
-                        'hero' => intval($citizen->getProfession()->getHeroic()),
-                        'name' => $citizen->getUser()->getName(),
-                        'avatar' => $citizen->getUser()->getAvatar() !== null ? $citizen->getUser()->getId() . "/" . $citizen->getUser()->getAvatar()->getFilename() . "." . $citizen->getUser()->getAvatar()->getFormat() : '',
-                        'id' => $citizen->getUser()->getId(),
-                        'ban' => intval($citizen->getBanished()),
-                        'job' => $citizen->getProfession()->getName() !== 'none' ? $citizen->getProfession()->getName() : '',
-                        'out' => intval($citizen->getZone() !== null),
-                        'baseDef' => $citizen->getAlive() ? $citizen->getHome()->getPrototype()->getDefense() : 0,
-                    ],
-                    'cdata_value' => $citizen->getHome()->getDescription()
-                ];
-                if (!$citizen->getTown()->getChaos()){
-                    $citizenNode['attributes']['x'] = $citizen->getZone() !== null ? $offset['x'] + $citizen->getZone()->getX() : $offset['x'];
-                    $citizenNode['attributes']['y'] = $citizen->getZone() !== null ? $offset['y'] - $citizen->getZone()->getY() : $offset['y'];
-                }
-                $data['data']['citizens']['list']['items'][] = $citizenNode;
-
-                if (!$citizen->getAlive()) {
+                if ($citizen->getAlias()) {
+                    $citizenNode = [
+                        'attributes' => [
+                            'dead' => (int)!$citizen->getAlive(),
+                            'hero' => intval($citizen->getProfession()->getHeroic()),
+                            'name' => $citizen->getUser()->getName(),
+                            'avatar' => $citizen->getUser()->getAvatar() !== null ? $citizen->getUser()->getId() . "/" . $citizen->getUser()->getAvatar()->getFilename() . "." . $citizen->getUser()->getAvatar()->getFormat() : '',
+                            'id' => $citizen->getUser()->getId(),
+                            'ban' => intval($citizen->getBanished()),
+                            'job' => $citizen->getProfession()->getName() !== 'none' ? $citizen->getProfession()->getName() : '',
+                            'out' => intval($citizen->getZone() !== null),
+                            'baseDef' => $citizen->getAlive() ? $citizen->getHome()->getPrototype()->getDefense() : 0,
+                        ],
+                        'cdata_value' => $citizen->getHome()->getDescription()
+                    ];
+                    if (!$citizen->getTown()->getChaos()){
+                        $citizenNode['attributes']['x'] = $citizen->getZone() !== null ? $offset['x'] + $citizen->getZone()->getX() : $offset['x'];
+                        $citizenNode['attributes']['y'] = $citizen->getZone() !== null ? $offset['y'] - $citizen->getZone()->getY() : $offset['y'];
+                    }
+                    $data['data']['citizens']['list']['items'][] = $citizenNode;
+                } else {
                     $cadaver = [
                         'attributes' => [
                             'name' => $citizen->getUser()->getName(),
