85a65fffecfb0cc04e97a0df4c6224f848d1881e
Benjamin Behrendt <wvsgnovize@gmail.com>
2023-03-12 17:08:45 +0100

    [WIP] Improve meta view

--- assets/ts/react/event-creator/modules/Meta.tsx
+++ assets/ts/react/event-creator/modules/Meta.tsx
@@ -29,7 +29,7 @@ export const HordesEventCreatorModuleMeta = ( {uuid}: {
             <h5>{ globals.strings.editor.title }</h5>
             <div className="row">
                 { meta === null && <div className="loading"></div> }
-                { meta !== null && ['en','fr','de','es'].map( lang => <div key={lang} className="padded cell rw-6 rw-sm-12">
+                { meta !== null && ['en','fr','de','es'].map( lang => <div key={lang} className="padded cell rw-12">
                     <HordesEventMetaEditor lang={lang} uuid={uuid} meta={ meta?.find( m => m.lang === lang ) ?? null } replace={(m:EventMeta|null) => {
                         let metaClone = [...meta];
                         if (m !== null) {
@@ -60,38 +60,41 @@ const HordesEventMetaEditor = ( {lang, uuid, meta, replace}: {
 
     return (
         <div className={`note ${meta === null ? 'pointer' : ''}`} onClick={ (meta === null && !editing) ? () => setEditing(true) : ()=>{} }>
-            { meta === null && !editing && <div className="center">
-                <Flag lang={lang}/><br/>
-                <span className="small">{ globals.strings.editor.add_meta.replace('{lang}', globals.strings.common.langs[lang]) }</span>
-            </div> }
-            { (meta !== null || editing) && <div className="row row-flex">
+            <div className="row row-flex">
                 <div className="cell padded-small"><Flag lang={lang} className="cell shrink-0" width={16}/></div>
                 <div className="cell grow-1">
-                    { !editing && <>
-                        <div className="small"><b>{ meta?.name }</b></div>
-                        <div className="small"><i>{ meta?.description }</i></div>
-                        <div className="row">
-                            <div className="padded cell"><button className="small" onClick={() => setEditing(true)}>{ globals.strings.common.edit }</button></div>
-                            <div className="padded cell"><button className="small" onClick={() => {
-                                replace(null);
-                                globals.api.deleteMeta( uuid, lang ).catch(() => replace(meta));
-                            }}>{ globals.strings.common.delete }</button></div>
-                        </div>
-                    </> }
-                    { editing && <>
-                        <input type="text" placeholder={globals.strings.editor.field_title} defaultValue={ meta?.name ?? '' } ref={editorTitle} />
-                        <textarea style={{resize: 'none'}} placeholder={globals.strings.editor.field_description} defaultValue={ meta?.description ?? '' } ref={editorDescription}></textarea>
-                        <div className="row">
-                            <div className="padded cell"><button className="small" onClick={() => setEditing(false)}>{ globals.strings.common.cancel }</button></div>
-                            <div className="padded cell right"><button onClick={() => {
-                                setEditing(false);
-                                globals.api.setMeta( uuid, lang, editorTitle.current.value, editorDescription.current.value ).then(m => replace(m.meta))
-                                replace({name: editorTitle.current.value, desc: editorDescription.current.value});
-                            }} className="small">{ globals.strings.common.save }</button></div>
-                        </div>
+                    { meta === null && !editing && <span className="small">{ globals.strings.editor.add_meta.replace('{lang}', globals.strings.common.langs[lang]) }</span> }
+                    { (meta !== null || editing) && <>
+                            { !editing && <>
+                                <div className="small"><b>{ meta?.name }</b></div>
+                                <div className="small"><i>{ meta?.description }</i></div>
+                            </> }
+                            { editing && <>
+                                <input type="text" placeholder={globals.strings.editor.field_title} defaultValue={ meta?.name ?? '' } ref={editorTitle} />
+                                <textarea style={{resize: 'none'}} placeholder={globals.strings.editor.field_description} defaultValue={ meta?.description ?? '' } ref={editorDescription}></textarea>
+                                <div className="row">
+                                    <div className="padded cell"><button className="small" onClick={() => setEditing(false)}>{ globals.strings.common.cancel }</button></div>
+                                    <div className="padded cell right"><button onClick={() => {
+                                        setEditing(false);
+                                        globals.api.setMeta( uuid, lang, editorTitle.current.value, editorDescription.current.value ).then(m => replace(m.meta))
+                                        replace({name: editorTitle.current.value, desc: editorDescription.current.value});
+                                    }} className="small">{ globals.strings.common.save }</button></div>
+                                </div>
+                            </> }
                     </> }
                 </div>
-            </div> }
+                { meta && !editing && <div className="cell shrink-0">
+                    <span className="cell padded-small shrink-0" title={globals.strings.common.edit}>
+                        <img className="pointer" alt={globals.strings.common.edit} src={globals.strings.common.edit_icon} onClick={() => setEditing(true)} />
+                    </span>
+                    <span className="cell padded-small shrink-0" title={globals.strings.common.delete}>
+                        <img className="pointer" alt={globals.strings.common.delete} src={globals.strings.common.delete_icon} onClick={() => {
+                            replace(null);
+                            globals.api.deleteMeta( uuid, lang ).catch(() => replace(meta));
+                        }} />
+                    </span>
+                </div> }
+            </div>
         </div>
     )
 };
