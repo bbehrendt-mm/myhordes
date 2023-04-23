78514d0c3b10d25712bac62f32c6a25377c2887a
Tim Docteur CAILLOT-GALLOIS <tim.caillot@gmail.com>
2023-04-23 09:56:09 +0200

    S1_V: Make code more readable by extracting blueprint logic

--- src/Service/NightlyHandler.php
+++ src/Service/NightlyHandler.php
@@ -330,19 +330,7 @@ class NightlyHandler
                         $this->log->debug("Citizen <info>{$citizen->getUser()->getUsername()}</info> survived camping at <info>{$citizen->getZone()->getX()}/{$citizen->getZone()->getY()}</info> with a survival chance of <info>" . ($survival_chance * 100) . "%</info>.");
 
                         // Grant blueprint if first camping on a ruin.
-                        if ($citizen->getZone()->getBlueprint() === Zone::BlueprintAvailable && $citizen->getZone()->getBuryCount() <= 0) {
-                            // Spawn BP.
-                            $bp_name = ($this->zone_handler->getZoneKm($citizen->getZone()) < 10)
-                                ? 'bplan_u_#00'
-                                : 'bplan_r_#00';
-                            $bp_item_prototype = $this->entity_manager->getRepository(ItemPrototype::class)->findOneBy(['name' => $bp_name]);
-                            $bp_item = $this->item_factory->createItem( $bp_item_prototype );
-                            $citizen->getZone()->getFloor()->addItem($bp_item);
-                            // Set zone blueprint.
-                            $citizen->getZone()->setBlueprint(Zone::BlueprintFound);
-                            $this->log->debug("Citizen <info>{$citizen->getUser()->getUsername()}</info> dropped a blueprint <info>{$bp_name}</info>.");
-
-                        }
+                        $this->grantBlueprintIfDeserved($citizen);
                     }
                 }
                 else {
@@ -1957,5 +1945,25 @@ class NightlyHandler
         $cc = $this->cleanup;
         $this->cleanup = [];
         return $cc;
+    }
+
+    /**
+     * @param mixed $citizen
+     * @return void
+     */
+    public function grantBlueprintIfDeserved(mixed $citizen): void
+    {
+        if ($citizen->getZone()->getBlueprint() === Zone::BlueprintAvailable && $citizen->getZone()->getBuryCount() <= 0) {
+            // Spawn BP.
+            $bp_name = ($this->zone_handler->getZoneKm($citizen->getZone()) < 10)
+                ? 'bplan_u_#00'
+                : 'bplan_r_#00';
+            $bp_item_prototype = $this->entity_manager->getRepository(ItemPrototype::class)->findOneBy(['name' => $bp_name]);
+            $bp_item = $this->item_factory->createItem($bp_item_prototype);
+            $citizen->getZone()->getFloor()->addItem($bp_item);
+            // Set zone blueprint.
+            $citizen->getZone()->setBlueprint(Zone::BlueprintFound);
+            $this->log->debug("Citizen <info>{$citizen->getUser()->getUsername()}</info> dropped a blueprint <info>{$bp_name}</info>.");
+        }
     }
 }
