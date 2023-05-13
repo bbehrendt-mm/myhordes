b93a4f32405be1fb3cfeea3ae17b7b5f54702a4f
Benjamin Behrendt <wvsgnovize@gmail.com>
2023-05-13 14:57:22 +0200

    Fix token exchange on attract page

--- assets/ts/toaster.ts
+++ assets/ts/toaster.ts
@@ -2,6 +2,11 @@ import {SecureStorage} from "./v2/security";
 window.addEventListener('load', () => {
     SecureStorage.acquire_token()
         .then(() => document.dispatchEvent(new Event('tokenExchangeCompleted')))
-        .catch(() => alert('Token exchange failure. Please reload the page.'))
+        .catch((e) => {
+            if (e === 'no_ticket') {
+                console.warn('No ticket.');
+                document.dispatchEvent(new Event('tokenExchangeCompleted'));
+            } else alert('Token exchange failure. Please reload the page.')
+        })
     ;
 }, {once: true, passive: true});
--- assets/ts/v2/security.ts
+++ assets/ts/v2/security.ts
@@ -4,12 +4,13 @@ export class SecureStorage {
 
     private static api_token: string = null;
 
-    public static token(): string { return this.api_token }
-    public static partial_token(): string { return this.api_token.slice(0,8) }
+    public static token(): string { return this.api_token ?? 'no_token_set_yet' }
+
+    public static partial_token(): string { return this.api_token?.slice(0,8) ?? 'no_token' }
 
     public static acquire_token(): Promise<boolean> {
         const ticket = ((document.getRootNode() as Document).children[0] as HTMLElement).dataset.ticket;
-        if (!ticket) return new Promise<boolean>((resolve,reject) => { reject(false); })
+        if (!ticket) return new Promise<boolean>((resolve,reject) => { reject('no_ticket'); })
 
         const fetch = new Fetch( 'user/security' );
         return (fetch.from(`/token/exchange/${ticket}/`)
