4f8c6f298b46c9976da33882c2380edd84df329a
Benjamin Behrendt <wvsgnovize@gmail.com>
2023-06-12 19:23:38 +0200

    Fix daily votes not being fully reset

--- src/Service/NightlyHandler.php
+++ src/Service/NightlyHandler.php
@@ -1263,9 +1263,9 @@ class NightlyHandler
         }
 
         foreach ($town->getCitizens() as $citizen) {
-
-            if ($citizen->getDailyUpgradeVote()) {
-                $this->cleanup[] = $citizen->getDailyUpgradeVote();
+            if ($vote = $citizen->getDailyUpgradeVote()) {
+                $this->cleanup[] = $vote;
+                $this->entity_manager->persist( $vote->getBuilding()->removeDailyUpgradeVote( $vote ) );
                 $citizen->setDailyUpgradeVote(null);
             }
 
