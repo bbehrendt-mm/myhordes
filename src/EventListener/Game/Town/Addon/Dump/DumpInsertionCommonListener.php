ee9512964e5216f1c72eafee6a4dd6efd83ba5b5
Ludofloria <ludofloria@aifesdespets.fr>
2023-09-29 15:11:09 +0200

    Don't calculate dumpable items of it's already filled

--- src/EventListener/Game/Town/Addon/Dump/DumpInsertionCommonListener.php
+++ src/EventListener/Game/Town/Addon/Dump/DumpInsertionCommonListener.php
@@ -81,6 +81,8 @@ final class DumpInsertionCommonListener implements ServiceSubscriberInterface
 	 * @throws NotFoundExceptionInterface
 	 */
     public function onCheckItems( DumpInsertionCheckEvent $event ): void {
+		if (!empty($event->dumpableItems)) return;
+
 		if ($event->citizen->getBanished() || !$event->dump_built)
 			$event->pushErrorCode( ErrorHelper::ErrorActionNotAvailable )->stopPropagation();
 
