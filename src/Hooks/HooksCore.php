b91e34407a0febd46af5a9183b6da12052f011f0
Ludofloria <ludofloria@aifesdespets.fr>
2023-10-10 00:23:48 +0200

    Added new hook

--- src/Hooks/HooksCore.php
+++ src/Hooks/HooksCore.php
@@ -2,8 +2,10 @@
 
 namespace App\Hooks;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
@@ -12,10 +14,15 @@ class HooksCore {
 	protected UrlGeneratorInterface $router;
 	protected Packages $assets;
 	protected Environment $twig;
-	public function __construct(TranslatorInterface $trans, UrlGeneratorInterface $router, Packages $assets, Environment $twig) {
+	protected TokenStorageInterface $tokenStorage;
+	protected ContainerInterface $container;
+
+	public function __construct(TranslatorInterface $trans, UrlGeneratorInterface $router, Packages $assets, Environment $twig, TokenStorageInterface $tokenStorage, ContainerInterface $container) {
 		$this->translator = $trans;
 		$this->router = $router;
 		$this->assets = $assets;
 		$this->twig = $twig;
+		$this->tokenStorage = $tokenStorage;
+		$this->container = $container;
 	}
 }
--- src/Twig/ExtensionsRuntime.php
+++ src/Twig/ExtensionsRuntime.php
@@ -4,8 +4,10 @@ namespace App\Twig;
 
 use App\Entity\Hook;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Asset\Packages;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 use Twig\Extension\RuntimeExtensionInterface;
@@ -16,6 +18,8 @@ class ExtensionsRuntime implements RuntimeExtensionInterface {
 	protected Packages $assets;
 	protected EntityManagerInterface $entityManager;
 	protected Environment $twig;
+	protected TokenStorageInterface $token;
+	protected ContainerInterface $container;
 
 	/**
 	 * @param TranslatorInterface    $translator
@@ -24,12 +28,14 @@ class ExtensionsRuntime implements RuntimeExtensionInterface {
 	 * @param EntityManagerInterface $entityManager
 	 * @param Environment            $twig
 	 */
-	public function __construct(TranslatorInterface $translator, UrlGeneratorInterface $router, Packages $assets, EntityManagerInterface $entityManager, Environment $twig) {
+	public function __construct(TranslatorInterface $translator, UrlGeneratorInterface $router, Packages $assets, EntityManagerInterface $entityManager, Environment $twig, TokenStorageInterface $token, ContainerInterface $container) {
 		$this->translator = $translator;
 		$this->router = $router;
 		$this->assets = $assets;
 		$this->entityManager = $entityManager;
 		$this->twig = $twig;
+		$this->token = $token;
+		$this->container = $container;
 	}
 
 	public function execute_hooks(string $hookName, ...$args): string {
@@ -45,7 +51,7 @@ class ExtensionsRuntime implements RuntimeExtensionInterface {
 			if (!class_exists($registeredHook->getClassname())) continue;
 
 			$className = $registeredHook->getClassname();
-			$hook = new $className($this->translator, $this->router, $this->assets, $this->twig);
+			$hook = new $className($this->translator, $this->router, $this->assets, $this->twig, $this->token, $this->container);
 			if (!is_callable([$hook, $hookFunction])) continue;
 
 			$output .= $hook->{$hookFunction}($args);
