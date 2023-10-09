27750557c96c6ab3b1a1e65c47e86576cb8b51c0
Ludofloria <ludofloria@aifesdespets.fr>
2023-10-09 17:11:56 +0200

    Added one more template

--- src/Hooks/DumpHooks.php
+++ src/Hooks/DumpHooks.php
@@ -2,21 +2,11 @@
 
 namespace App\Hooks;
 
-use App\Entity\ItemPrototype;
-
 class DumpHooks extends HooksCore {
 
 	function hookDumpDisplayCost(...$args): string {
-		$output = "";
 		$ap_cost = $args[0][0];
-		if ($ap_cost > 0) {
-			$output .= '<div class="help warning">';
-			$output .= "<b>" . $this->translator->trans('Achtung', [],'global'). " :</b> ";
-			$output .= $this->translator->trans('Jeder so zerstörte Gegenstand kostet dich {ap}.', ['{ap}' => '<div class="ap">' . $ap_cost . '</div>'],'game');
-			$output .= '<a class="help-button"><div class="tooltip help">' . $this->translator->trans('Diese Kosten können durch den Aufbau der <strong>Müll für Alle</strong>.',[],'global') . "</div>". $this->translator->trans('Hilfe', [],'global') . "</a>";
-			$output .= '</div>';
-		}
-		return $output;
+		return $this->twig->render('partials/hooks/dump/display_cost.html.twig', ['ap_cost' => $ap_cost]);
 	}
 
 	function hookDumpDisplayItems(...$args): string {
