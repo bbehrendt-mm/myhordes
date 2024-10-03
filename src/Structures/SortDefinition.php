0262615de006894458945e55bbe0c2cf567c0c9c
Benjamin Behrendt <wvsgnovize@gmail.com>
2024-10-03 14:58:33 +0200

    Fix camping

--- src/Structures/SortDefinition.php
+++ src/Structures/SortDefinition.php
@@ -18,4 +18,32 @@ class SortDefinition
             $this->reference = null;
     }
 
+    public static function atStart( int $priority = 0 ): SortDefinition {
+        return new SortDefinition(SortDefinitionWord::Start, priority: $priority);
+    }
+
+    public static function atEnd( int $priority = 0 ): SortDefinition {
+        return new SortDefinition(SortDefinitionWord::End, priority: $priority);
+    }
+
+    public static function before( string $className, int $priority = 0 ): SortDefinition {
+        return new SortDefinition(SortDefinitionWord::Before, $className, $priority);
+    }
+
+    public static function after( string $className, int $priority = 0 ): SortDefinition {
+        return new SortDefinition(SortDefinitionWord::After, $className, $priority);
+    }
+
+    public function stableSort(SortDefinition $b): int {
+        if (!$this->word->stable() || !$b->word->stable()) return 0;
+
+        if ($this->word === $b->word) return $b->priority <=> $this->priority;
+        if ($this->word === SortDefinitionWord::Start) return -1;
+        if ($b->word === SortDefinitionWord::Start) return 1;
+        if ($this->word === SortDefinitionWord::End) return 1;
+        if ($b->word === SortDefinitionWord::End) return -1;
+
+        return 0;
+    }
+
 }
