1c96819390c9ec052d5ae8f1a5720b9fc2817cc5
Benjamin Behrendt <wvsgnovize@gmail.com>
2025-03-29 17:43:44 +0100

    Update translations

--- src/Command/Translation/UpdateTranslationsCommand.php
+++ src/Command/Translation/UpdateTranslationsCommand.php
@@ -54,7 +54,6 @@ class UpdateTranslationsCommand extends Command
         $array = [
             'locale' => $lang,
             '--force' => true,
-            '--sort' => 'asc',
             '--format' => 'yml',
             '--prefix' => '',
         ];
