65b6da3baf90643f2bd4712fbbe0151bdf062d02
Benjamin Behrendt <wvsgnovize@gmail.com>
2025-03-29 14:45:23 +0100

    Adjustments for XP earnings

--- src/Hooks/SoulHooks.php
+++ src/Hooks/SoulHooks.php
@@ -5,6 +5,7 @@ namespace App\Hooks;
 use App\Entity\CitizenProfession;
 use App\Entity\PictoPrototype;
 use App\Entity\User;
+use App\Service\Actions\XP\GetPictoXPStepsAction;
 use App\Service\User\UserUnlockableService;
 
 class SoulHooks extends HooksCore {
@@ -24,6 +25,9 @@ class SoulHooks extends HooksCore {
 	public function earnXHP(): string {
         /** @var User $user */
         $user = $this->tokenStorage->getToken()->getUser();
+
+        /** @var GetPictoXPStepsAction $action */
+        $action = $this->container->get(GetPictoXPStepsAction::class);
 
         $day_data = [
             [
@@ -78,60 +82,7 @@ class SoulHooks extends HooksCore {
             'r_suhard_#00' => [5 => [null, 7]],
         ];
 
-        $pt_2 = [ 1 => 2, 3 => 1, 5 => 1, 8 => 1, 10 => 1 ];
-        $pt_5 = [ 1 => 5, 3 => 2, 5 => 2, 8 => 2, 10 => 2 ];
-        $pt_7 = [ 1 => 7, 3 => 2, 5 => 2, 8 => 2, 10 => 2 ];
-        $pt_2_6 = [ 6 => 2, 12 => 1, 18 => 1, 24 => 1 ];
-        $pt_2_10 = [ 10 => 2, 20 => 1, 30 => 1, 50 => 1 ];
-        $pt_2_15 = [ 15 => 2, 30 => 1, 45 => 1, 60 => 1 ];
-
-        $p_job = [50 => 4, 100 => 7];
-
-        $picto_db = [
-            'r_thermal_#00' => $pt_2,
-            'r_ebcstl_#00' =>  $pt_2,
-            'r_ebpmv_#00' =>   $pt_2,
-            'r_ebgros_#00' =>  $pt_2,
-            'r_ebcrow_#00' =>  $pt_2,
-            'r_maso_#00'   =>  $pt_2,
-            'r_wondrs_#00' =>  [15 => 2, 30 => 1, 45 => 1, 60 => 1],
-
-            'r_batgun_#00' =>  $pt_5,
-            'r_door_#00'   =>  $pt_5,
-            'r_explo2_#00' =>  $pt_5,
-            'r_ebuild_#00' =>  $pt_5,
-            'r_chstxl_#00' =>  [ 1 => 5, 2 => 2, 3 => 2, 5 => 2 ],
-
-            'r_dnucl_#00'  =>  $pt_7,
-            'r_watgun_#00' =>  $pt_7,
-            'r_cmplst_#00' =>  $pt_7,
-
-            'r_tronco_#00' =>  [ 1 => 8, 2 => 2, 3 => 2, 5 => 2 ],
-
-            'r_cobaye_#00' =>  $pt_2_6,
-            'r_solban_#00' =>  $pt_2_6,
-            'r_explor_#00' =>  $pt_2_6,
-            'r_collec_#00' =>  $pt_2_6,
-            'r_guard_#00'  =>  $pt_2_6,
-            'r_ruine_#00'  =>  $pt_2_6,
-
-            'r_repair_#00' =>  $pt_2_10,
-            'r_plundr_#00' =>  $pt_2_10,
-            'r_camp_#00'   =>  $pt_2_10,
-            'r_digger_#00' =>  $pt_2_10,
-
-            'r_theft_#00'  =>  $pt_2_15,
-            'r_cgarb_#00'  =>  $pt_2_15,
-
-            'r_jbasic_#00' =>  $p_job,
-            'r_jtamer_#00' =>  $p_job,
-            'r_jrangr_#00' =>  $p_job,
-            'r_jermit_#00' =>  $p_job,
-            'r_jcolle_#00' =>  $p_job,
-            'r_jguard_#00' =>  $p_job,
-            'r_jtech_#00'  =>  $p_job,
-        ];
-
+        $picto_db = ($action)()->toArray();
         $picto_data = [];
 
         foreach ($multi_picto_db as $id => $points) {
