7a63a09832b276bc0c43ef6c1831f36cfffbd4fe
Benjamin Behrendt <wvsgnovize@gmail.com>
2026-02-08 19:27:24 +0100

    Deploy total exploration picto before d5

--- src/Service/TownHandler.php
+++ src/Service/TownHandler.php
@@ -813,7 +813,7 @@ class TownHandler
 
         foreach ($town->getCitizens() as $citizen) {
             if (!$citizen->getAlive()) continue;
-            $this->picto_handler->give_picto($citizen, $pictoPrototype);
+            $this->picto_handler->give_validated_picto($citizen, $pictoPrototype);
         }
 
         $town->setFullyExploredAwarded(true);
