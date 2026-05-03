1c08dbf42e751dab0fcaf324839d5fd31a78b20e
Benjamin Behrendt <wvsgnovize@gmail.com>
2026-05-03 17:19:18 +0200

    Remove duplicate code

--- src/Event/Common/Social/SponsorshipData.php
+++ src/Event/Common/Social/SponsorshipData.php
@@ -25,30 +25,3 @@ readonly class SponsorshipData
         $this->newcomer = $newcomer;
     }
 }
-<?php
-
-namespace App\Event\Common\Social;
-
-use App\Entity\User;
-
-/**
- * Data payload for a new sponsorship registration.
- *
- * @property-read User $sponsor  The existing player whose refer-link was used.
- * @property-read User $newcomer The newly registered player.
- */
-readonly class SponsorshipData
-{
-    public User $sponsor;
-    public User $newcomer;
-
-    /**
-     * @param User $sponsor  The existing player whose refer-link was used.
-     * @param User $newcomer The newly registered player.
-     */
-    public function setup(User $sponsor, User $newcomer): void
-    {
-        $this->sponsor  = $sponsor;
-        $this->newcomer = $newcomer;
-    }
-}
--- src/Event/Common/Social/SponsorshipEvent.php
+++ src/Event/Common/Social/SponsorshipEvent.php
@@ -17,22 +17,3 @@ class SponsorshipEvent extends Event
         return SponsorshipData::class;
     }
 }
-<?php
-
-namespace App\Event\Common\Social;
-
-use App\Event\Event;
-
-/**
- * Fired after a new player successfully registers via a sponsor link.
- *
- * @property-read SponsorshipData $data
- * @mixin SponsorshipData
- */
-class SponsorshipEvent extends Event
-{
-    protected static function configuration(): string
-    {
-        return SponsorshipData::class;
-    }
-}
