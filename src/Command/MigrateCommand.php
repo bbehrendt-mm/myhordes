e80dfa2bf973900a592a71316c4a578430777110
Benjamin Behrendt <wvsgnovize@gmail.com>
2026-06-01 22:39:57 +0200

    Fix initial migration version creation

--- src/Command/MigrateCommand.php
+++ src/Command/MigrateCommand.php
@@ -395,7 +395,7 @@ class MigrateCommand extends Command
 
             $fixed_migrations = ['Version20250830134338', 'Version20260215154040'];
             foreach ($fixed_migrations as $migration)
-                if (!$this->helper->capsule( "doctrine:migrations:version \"DoctrineMigrations\\{$migration}\" --add", $output )) {
+                if (!$this->helper->capsule( "doctrine:migrations:version \"DoctrineMigrations\\{$migration}\" --add --silent", $output )) {
                     $output->writeln("<error>Unable to create schema.</error>");
                     return 2;
                 }
